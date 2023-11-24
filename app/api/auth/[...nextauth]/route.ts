5b10118fd556f1fd9ce21f6923e042513fbdfc82
BishwasKoirala <wasubisu69@gmail.com>
2023-11-24 23:21:53 +0900

    just did some silly mistakes ,lol but it works vey well

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,37 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-// this may be out to date
-const handler =  NextAuth({
-  providers : [
+
+const handler = NextAuth({
+  providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+      clientSecret:process.env.GOOGLE_CLIENT_SECRET!
     })
   ]
 });
 
-export { handler as GET , handler as Post}
+export { handler as GET , handler as POST}
+
+
+
+
+
+
+
+
+
+
+// import GoogleProvider from "next-auth/providers/google";
+
+// // this may be out to date
+// const handler =  NextAuth({
+//   providers : [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID!,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+//     })
+//   ]
+// });
+
+// export { handler as GET , handler as Post}
