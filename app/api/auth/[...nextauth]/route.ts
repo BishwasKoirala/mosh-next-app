291120a26ad37727bf93add309f7981fc06ee5f1
BishwasKoirala <wasubisu69@gmail.com>
2023-11-24 23:22:17 +0900

    committ nasd

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -13,25 +13,3 @@ const handler = NextAuth({
 
 export { handler as GET , handler as POST}
 
-
-
-
-
-
-
-
-
-
-// import GoogleProvider from "next-auth/providers/google";
-
-// // this may be out to date
-// const handler =  NextAuth({
-//   providers : [
-//     GoogleProvider({
-//       clientId: process.env.GOOGLE_CLIENT_ID!,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET!
-//     })
-//   ]
-// });
-
-// export { handler as GET , handler as Post}
