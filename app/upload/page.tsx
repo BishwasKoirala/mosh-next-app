4139e75ce3b8fd3cd679f8e5ab1408587f8a44a1
BishwasKoirala <wasubisu69@gmail.com>
2023-11-24 16:07:13 +0900

    adsfa

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -15,6 +15,14 @@ const uploadPage = () => {
     {publicId && 
     <CldImage src={publicId} width={270} height={180} alt ="A code note image"/>}
       <CldUploadWidget uploadPreset='rmui9qyj'
+      options={{
+        sources: ['local'],
+        multiple: false ,
+        maxFiles: 5,
+        styles: {
+          
+        }
+      }}
       onUpload={(result , widget) => {
         if (result.event !== 'success') return
         const info = result.info as CloudinaryResult;
